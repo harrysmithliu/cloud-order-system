7ce4f30d84a1982c7b80625cea4e2c7d52a5d2ca
harrysmithliu <harrysmithliu@gmail.com>
2025-11-27 14:38:28 -0500

    feat: Gateway successfully forwarding requests to user-service; controller reached but returning 401 UNAUTHORIZED due to "Invalid credentials"

--- common-web-security/src/main/java/com/harry/security/jwt/JwtAuthenticationFilter.java
+++ common-web-security/src/main/java/com/harry/security/jwt/JwtAuthenticationFilter.java
@@ -43,4 +43,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         filterChain.doFilter(request, response);
     }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getServletPath();
+        return path.startsWith("/api/auth/login");
+    }
+
 }
--- user-service/src/main/java/com/harry/user/config/UserSecurityConfig.java
+++ user-service/src/main/java/com/harry/user/config/UserSecurityConfig.java
@@ -1,23 +1,35 @@
 package com.harry.user.config;
 
+import com.harry.security.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class UserSecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public UserSecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
     @Bean
-    public SecurityFilterChain userSecurity(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/login").permitAll() // let go login request
+                        .requestMatchers("/api/auth/login").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(b -> b.disable());
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .httpBasic(basic -> basic.disable());
 
         return http.build();
     }
